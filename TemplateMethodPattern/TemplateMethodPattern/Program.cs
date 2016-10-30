b1260665a9b677a7018ba5793a9c3d644e051e3d
toshi0607 <s.toshi0607@gmail.com>
2016-10-30 17:28:55 +0900

    add template method pattern

---
+++ TemplateMethodPattern/TemplateMethodPattern/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TemplateMethodPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public abstract class AbstractDisplay
+    {
+        public abstract void Open();
+        public abstract void Print();
+        public abstract void Close();
+        public void Display()
+        {
+            this.Open();
+            for(int i = 0; i < 5; i++)
+            {
+                this.Print();
+            }
+            this.Close();
+        }
+    }
+
+    public class CharDisplay : AbstractDisplay
+    {
+        private char Ch { get; set; }
+        public CharDisplay(char ch)
+        {
+            this.Ch = ch;
+        }
+        public override void Open()
+        {
+            Console.WriteLine("<<");
+        }
+        public override void Print()
+        {
+            Console.WriteLine(Ch);
+        }
+        public override void Close()
+        {
+            Console.WriteLine(">>");
+        }
+
+    }
+
+}
