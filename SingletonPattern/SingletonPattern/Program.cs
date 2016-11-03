3a39bfd5893b964dc4d043492ee1ffc575019f89
toshi0607 <s.toshi0607@gmail.com>
2016-11-03 14:20:23 +0900

    add user

--- SingletonPattern/SingletonPattern/Program.cs
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -10,6 +10,26 @@ namespace SingletonPattern
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Start.");
+            // => Start.
+            Singleton obj1 = Singleton.GetInstance();
+            // => インスタンスを生成しました。
+            // => 
+            Singleton obj2 = Singleton.GetInstance();
+            if (obj1 == obj2)
+            {
+                Console.WriteLine("obj1とobj2は同じインスタンスです。");
+                // => obj1とobj2は同じインスタンスです。
+            }
+            else
+            {
+                Console.WriteLine("obj1とobj2は同じ違うインスタンスです。");
+            }
+            Console.WriteLine("End.");
+            // => End.
+
+            // 実行が一瞬で終わって確認できないので、キーの入力を待ちます
+            Console.ReadLine();
         }
     }
 
