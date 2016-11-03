87e8c846708c04f0a1af6a29fd96b6a9c4cb03db
toshi0607 <s.toshi0607@gmail.com>
2016-11-03 14:14:19 +0900

    add singleton pattern

---
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Singleton
+    {
+        private static Singleton singleton = new Singleton();
+        private Singleton()
+        {
+            Console.WriteLine("インスタンスを生成しました。");
+        }
+        public static Singleton GetInstance()
+        {
+            return singleton;
+        }
+    }
+}
