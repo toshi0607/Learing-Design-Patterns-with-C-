008ef18b90f3527d3be8db4d6944356083ac61ed
toshi0607 <s.toshi0607@gmail.com>
2016-11-03 11:45:15 +0900

    add factory method pattern

---
+++ FactoryMethodPattern/FactoryMethodPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+namespace Framework
+{
+    public abstract class Product
+    {
+        public abstract void Use();
+    }
+
+    public abstract class Factory
+    {
+        public Product Create(string owner)
+        {
+            Product p = CreateProduct(owner);
+            RegisterProduct(p);
+            return p;
+        }
+        protected abstract Product CreateProduct(string owner);
+        protected abstract void RegisterProduct(Product product);
+    }
+}
