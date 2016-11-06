d986e33c4381f0c5a244d026b0e5717bcf2b0541
toshi0607 <s.toshi0607@gmail.com>
2016-11-06 17:18:07 +0900

    add prototype pattern

---
+++ PrototypePattern/PrototypePattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+namespace Framework
+{
+    public interface Product : ICloneable
+    {
+        void Use(string s);
+        Product createClone();
+    }
+}
