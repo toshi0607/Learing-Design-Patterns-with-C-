369a5281658591cd121ccdc297576193984212f3
toshi0607 <s.toshi0607@gmail.com>
2016-11-15 23:06:54 +0900

    add text builder

--- BuilderPattern/BuilderPattern/Program.cs
+++ BuilderPattern/BuilderPattern/Program.cs
@@ -48,4 +48,39 @@ namespace BuilderPattern
             this.builder.Close();
         }
     }
+
+    public class TextBuilder : Builder
+    {
+        private StringBuilder sb = new StringBuilder();
+        public string Result
+        {
+            get { return sb.ToString(); }
+        }
+        public override void MakeTitle(string title)
+        {
+            sb.Append("==============================\n");
+            sb.Append($"『{title}』");
+            sb.Append("\n");
+        }
+
+        public override void MakeString(string str)
+        {
+            sb.Append($"■{str}\n");
+            sb.Append("\n");
+        }
+
+        public override void MakeItems(string[] items)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                sb.Append($"　・{items[i]}\n");
+            }
+            sb.Append("\n");
+        }
+
+        public override void Close()
+        {
+            sb.Append("==============================\n");
+        }
+    }
 }
