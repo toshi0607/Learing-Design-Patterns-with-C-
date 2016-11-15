8590706b4c26c08b11fade13caf90e5bb0c6b4e1
toshi0607 <s.toshi0607@gmail.com>
2016-11-15 22:34:43 +0900

    add builder

---
+++ BuilderPattern/BuilderPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public abstract class Builder
+    {
+        public abstract void MakeTitle(string title);
+        public abstract void MakeString(string str);
+        public abstract void MakeItems(string[] items);
+        public abstract void Close();
+    }
+}
