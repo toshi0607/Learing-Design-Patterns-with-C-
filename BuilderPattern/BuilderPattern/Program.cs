02911e24359cec702c14df4735bc176071bb1dc0
toshi0607 <s.toshi0607@gmail.com>
2016-11-15 22:47:03 +0900

    add director

--- BuilderPattern/BuilderPattern/Program.cs
+++ BuilderPattern/BuilderPattern/Program.cs
@@ -20,4 +20,32 @@ namespace BuilderPattern
         public abstract void MakeItems(string[] items);
         public abstract void Close();
     }
+
+    public class Director
+    {
+        private Builder builder;
+        public Director(Builder builder)
+        {
+            this.builder = builder;
+        }
+
+        public void Construct()
+        {
+            this.builder.MakeTitle("Greeting");
+            this.builder.MakeString("朝から昼にかけて");
+            this.builder.MakeItems(new string[]
+            {
+                "おはようございます。",
+                "こんにちは。",
+            });
+            this.builder.MakeString("夜に");
+            this.builder.MakeItems(new string[]
+            {
+                "こんばんは。",
+                "おやすみなさい。",
+                "さようなら。",
+            });
+            this.builder.Close();
+        }
+    }
 }
