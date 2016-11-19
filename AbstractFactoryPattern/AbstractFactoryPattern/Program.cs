2f95d7c3cdc60c26944e5a7ecd6d64a3c8b152b1
toshi0607 <s.toshi0607@gmail.com>
2016-11-19 15:31:15 +0900

    add tray class

--- AbstractFactoryPattern/AbstractFactoryPattern/Program.cs
+++ AbstractFactoryPattern/AbstractFactoryPattern/Program.cs
@@ -34,4 +34,15 @@ namespace Factory
             this.url = url;
         }
     }
+
+    public abstract class Tray : Item
+    {
+        protected List<Item> tray = new List<Item>();
+        public Tray(string caption) : base(caption) { }
+
+        public void Add(Item item)
+        {
+            tray.Add(item);
+        }
+    }
 }
