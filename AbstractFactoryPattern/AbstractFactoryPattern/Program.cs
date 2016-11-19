cd9f32e2fb2583873e31c9593ef9ee34602d1b69
toshi0607 <s.toshi0607@gmail.com>
2016-11-19 17:54:48 +0900

    add factory class

--- AbstractFactoryPattern/AbstractFactoryPattern/Program.cs
+++ AbstractFactoryPattern/AbstractFactoryPattern/Program.cs
@@ -73,9 +73,34 @@ namespace Factory
             }
             catch(IOException e)
             {
-                Console.WriteLine(e.StackTrace);
+                Console.Error.WriteLine(e.StackTrace);
             }
         }
         public abstract string MakeHTML();
+    }
+
+    public abstract class Factory
+    {
+        public static Factory GetFactory(string classname)
+        {
+            Factory factory = null;
+            try
+            {
+                Type type = Type.GetType("classname");
+                factory = (Factory)Activator.CreateInstance(type);
+            }
+            catch(TypeLoadException)
+            {
+                Console.Error.WriteLine($"クラス{classname}が見つかりません。");
+            }
+            catch(Exception e)
+            {
+                Console.Error.WriteLine(e.StackTrace);
+            }
+            return factory;
+        }
+        public abstract Link CreateLink(string caption, string url);
+        public abstract Tray CreateTray(string caption);
+        public abstract Page CreatePage(string title, string author);
     }
 }
