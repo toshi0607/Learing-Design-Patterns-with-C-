1ef031e1274f46064549fc85a62e13461063f0ee
toshi0607 <s.toshi0607@gmail.com>
2016-11-19 18:23:49 +0900

    add ListLink class

--- AbstractFactoryPattern/AbstractFactoryPattern/Program.cs
+++ AbstractFactoryPattern/AbstractFactoryPattern/Program.cs
@@ -4,10 +4,45 @@ using System.IO;
 
 namespace AbstractFactoryPattern
 {
+    using Factory;
     class Program
     {
         static void Main(string[] args)
         {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Usage: C# Main class.name.of.ConcreteFactory");
+                Console.WriteLine("Example 1: C# Main listfactory.ListFactory");
+                Console.WriteLine("Example 2: C# Main tablefactory.TableFactory");
+                Environment.Exit(0);
+            }
+            Factory factory = Factory.GetFactory(args[0]);
+
+            Link asahi = factory.CreateLink("朝日新聞", "http://www.asashi.com/");
+            Link yomiuri = factory.CreateLink("読売新聞", "http://www.yomiuri.co.jp/");
+
+            Link usYahoo = factory.CreateLink("Yahoo!", "http://www.yahoo.com/");
+            Link jpYahoo = factory.CreateLink("Yahoo!Japan", "http://www.yahoo.co.jp/");
+            Link excite = factory.CreateLink("Excite", "http://www.excite.co.jp/");
+            Link google = factory.CreateLink("Google", "http://www.google.com/");
+
+            Tray traynews = factory.CreateTray("新聞");
+            traynews.Add(asahi);
+            traynews.Add(yomiuri);
+
+            Tray trayyahoo = factory.CreateTray("Yahoo!");
+            trayyahoo.Add(usYahoo);
+            trayyahoo.Add(jpYahoo);
+
+            Tray traysearch = factory.CreateTray("サーチエンジン");
+            traysearch.Add(trayyahoo);
+            traysearch.Add(excite);
+            traysearch.Add(google);
+
+            Page page = factory.CreatePage("LinkPage", "結城 浩");
+            page.Add(traynews);
+            page.Add(traysearch);
+            page.Output();
         }
     }
 }
@@ -102,5 +137,19 @@ namespace Factory
         public abstract Link CreateLink(string caption, string url);
         public abstract Tray CreateTray(string caption);
         public abstract Page CreatePage(string title, string author);
+    }
+}
+
+namespace ListFactory
+{
+    using Factory;
+    public class ListLink : Link
+    {
+        public ListLink(string caption, string url) : base(caption, url) { }
+
+        public override string MakeHTML()
+        {
+            return $" <li><a href=\"{url}>\"{caption}</a></li>\n";
+        }
     }
 }
