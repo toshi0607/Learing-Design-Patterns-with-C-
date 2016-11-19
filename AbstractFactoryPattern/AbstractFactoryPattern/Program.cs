a4374fa3bc3e4764aa52c84101fb445e11e925c6
toshi0607 <s.toshi0607@gmail.com>
2016-11-19 15:07:43 +0900

    add abstract factory pattern

---
+++ AbstractFactoryPattern/AbstractFactoryPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+namespace Factory
+{
+    public abstract class Item
+    {
+        protected string caption;
+        public Item(string caption)
+        {
+            this.caption = caption;
+        }
+        public abstract string MakeHTML();
+    }
+}
