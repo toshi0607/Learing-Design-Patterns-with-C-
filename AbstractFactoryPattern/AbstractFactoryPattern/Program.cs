34816dc801905baabe99944fa46d3f60b93eb989
toshi0607 <s.toshi0607@gmail.com>
2016-11-19 22:45:23 +0900

    add TableTray class

--- AbstractFactoryPattern/AbstractFactoryPattern/Program.cs
+++ AbstractFactoryPattern/AbstractFactoryPattern/Program.cs
@@ -259,5 +259,28 @@ namespace TableFactory
                 return $"<td><a href=\"{url}\">{caption}</a></td>\n";
             }
         }
+
+        public class TableTray : Tray
+        {
+            public TableTray(string caption) : base(caption) { }
+
+            public override string MakeHTML()
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<td>");
+                sb.Append("<table width=\"100%\"> border=\"1\"><tr>");
+                sb.Append($"<td bgcolor=\"#cccccc\" align=\"center\" colspan=\"{tray.Count}\"<b>{caption}</b></td>");
+                sb.Append("</tr>\n");
+                sb.Append("<tr>\n");
+                IEnumerator<Item> e = tray.GetEnumerator();
+                while(e.MoveNext())
+                {
+                    sb.Append(e.Current.MakeHTML());
+                }
+                sb.Append("<tr></table>");
+                sb.Append("</tr>");
+                return sb.ToString();
+            }
+        }
     }
 }
