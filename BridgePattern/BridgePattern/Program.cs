77d9cb774c6765561b9ceb6b5016ebaa8e81c4d3
toshi0607 <s.toshi0607@gmail.com>
2016-11-23 13:28:46 +0900

    add CountDisplay class

--- BridgePattern/BridgePattern/Program.cs
+++ BridgePattern/BridgePattern/Program.cs
@@ -44,5 +44,18 @@ namespace BridgePattern
         }
     }
     
+    public class CountDisplay : Display
+    {
+        public CountDisplay(DisplayImpl impl) : base(impl) { }
 
+        public void MultiDisplay(int times)
+        {
+            Open();
+            for (int i = 0; i < times; i++)
+            {
+                Print();
+            }
+            Close();
+        }
+    }
 }
