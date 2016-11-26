29f4475f511945bac6864db311b05eb5cfd5cd45
toshi0607 <s.toshi0607@gmail.com>
2016-11-26 22:09:17 +0900

    add WinningStrategy class

--- StrategyPattern/StrategyPattern/Program.cs
+++ StrategyPattern/StrategyPattern/Program.cs
@@ -73,4 +73,30 @@ namespace StrategyPattern
         Hand NextHand();
         void Study(bool win); 
     }
+
+    public class WinningStrategy : Strategy
+    {
+        private Random random;
+        private bool won = false;
+        private Hand prevHand;
+        public WinningStrategy(int seed)
+        {
+            random = new Random(seed);
+        }
+
+        public Hand NextHand()
+        {
+            if (!won)
+            {
+                prevHand = Hand.GetHand(random.Next(3));
+            }
+            return prevHand;
+        }
+
+        public void Study(bool win)
+        {
+            won = win;
+        }
+    }
+
 }
