8d86532ab6a00ee887abe5ceba5a4b25650daf8a
toshi0607 <s.toshi0607@gmail.com>
2016-11-27 22:15:51 +0900

    add Player class]

--- StrategyPattern/StrategyPattern/Program.cs
+++ StrategyPattern/StrategyPattern/Program.cs
@@ -160,4 +160,46 @@ namespace StrategyPattern
             }
         }
     }
+
+    public class Player
+    {
+        private string name;
+        private Strategy strategy;
+        private int wincount;
+        private int losecount;
+        private int gamecount;
+        public Player(string name, Strategy strategy)
+        {
+            this.name = name;
+            this.strategy = strategy;
+        }
+
+        public Hand NextHand()
+        {
+            return strategy.NextHand();
+        }
+
+        public void Win()
+        {
+            strategy.Study(true);
+            wincount++;
+        }
+
+        public void Lose()
+        {
+            strategy.Study(false);
+            losecount++;
+            gamecount++;
+        }
+
+        public void Even()
+        {
+            gamecount++;
+        }
+
+        public override string ToString()
+        {
+            return $"[{name}:{gamecount} games, {wincount} win, {losecount} lose]";
+        }
+    }
 }
