9ac4e0e2c722e00e11e59dab5053275a2d6f95e6
toshi0607 <s.toshi0607@gmail.com>
2016-11-27 23:35:37 +0900

    add main

--- StrategyPattern/StrategyPattern/Program.cs
+++ StrategyPattern/StrategyPattern/Program.cs
@@ -10,6 +10,45 @@ namespace StrategyPattern
     {
         static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: Input  two args as radomeseed1 ane randomeseed2");
+                Console.WriteLine("Example: 314 15");
+                Environment.Exit(0);
+            }
+            int seed1 = int.Parse(args[0]);
+            int seed2 = int.Parse(args[1]);
+            Player player1 = new Player("Taro", new WinningStrategy(seed1));
+            Player player2 = new Player("Hanako", new ProbStrategy(seed2));
+            for (int i = 0; i < 10000; i++)
+            {
+                Hand nextHand1 = player1.NextHand();
+                Hand nextHand2 = player2.NextHand();
+                if (nextHand1.IsStrongerThan(nextHand2))
+                {
+                    Console.WriteLine($"Winner: {player1}");
+                    player1.Win();
+                    player2.Lose();
+                }
+                else if (nextHand2.IsStrongerThan(nextHand1))
+                {
+                    Console.WriteLine($"Winner: {player2}");
+                    player1.Lose();
+                    player2.Win();
+                }
+                else
+                {
+                    Console.WriteLine("Even...");
+                    player1.Even();
+                    player2.Even();
+                }
+
+                Console.WriteLine("Total result:");
+                Console.WriteLine(player1.ToString());
+                Console.WriteLine(player2.ToString());
+            }
+            // 実行が一瞬で終わって確認できないので、キーの入力を待ちます
+            Console.ReadLine();
         }
     }
 
@@ -183,6 +222,7 @@ namespace StrategyPattern
         {
             strategy.Study(true);
             wincount++;
+            gamecount++;
         }
 
         public void Lose()
