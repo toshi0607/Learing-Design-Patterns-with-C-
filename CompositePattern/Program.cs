78b3de88050798829aa28836070147752b3e2545
toshi0607 <s.toshi0607@gmail.com>
2016-12-04 22:36:36 +0900

    add Entry class

--- CompositePattern/Program.cs
+++ CompositePattern/Program.cs
@@ -12,4 +12,26 @@ namespace CompositePattern
         {
         }
     }
+
+    public abstract class Entry
+    {
+        public string Name { get; }
+        public int Size { get; }
+        public virtual Entry Add(Entry entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintList()
+        {
+            PrintList("");
+        }
+
+        protected abstract void PrintList(string prefix);
+
+        public override string ToString()
+        {
+            return $"{Name} ({Size})";
+        }
+    }
 }
