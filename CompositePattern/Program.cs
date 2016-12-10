26550731c9b1c60919fc67ce968d4700ae21fda9
toshi0607 <s.toshi0607@gmail.com>
2016-12-10 21:30:36 +0900

    add role description

--- CompositePattern/Program.cs
+++ CompositePattern/Program.cs
@@ -8,8 +8,46 @@ namespace CompositePattern
 {
     class Program
     {
+        // Client
+        // ・Compositeパターンの利用者
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Making root entries...");
+                Directry rootdir = new Directry("root");
+                Directry bindir = new Directry("bin");
+                Directry tmpdir = new Directry("tmp");
+                Directry usrdir = new Directry("usr");
+                rootdir.Add(bindir);
+                rootdir.Add(tmpdir);
+                rootdir.Add(usrdir);
+                bindir.Add(new File("vi", 10000));
+                bindir.Add(new File("latex", 20000));
+                rootdir.PrintList();
+
+                Console.WriteLine("");
+                Console.WriteLine("Making user entries...");
+                Directry nyanchu = new Directry("nyanchu");
+                Directry nc = new Directry("nc");
+                Directry toshi0607 = new Directry("toshi0607");
+                usrdir.Add(nyanchu);
+                usrdir.Add(nc);
+                usrdir.Add(toshi0607);
+                nyanchu.Add(new File("diary.html", 100));
+                nyanchu.Add(new File("composite.cs", 200));
+                nc.Add(new File("memo.txt", 300));
+                toshi0607.Add(new File("game.doc", 400));
+                toshi0607.Add(new File("junk.mail", 500));
+                rootdir.PrintList();
+            }
+            catch (FileTreatmentException e)
+            {
+                Console.Error.WriteLine(e.StackTrace);
+            }
+
+            // 実行が一瞬で終わって確認できないので、キーの入力を待ちます
+            Console.ReadLine();
         }
     }
 
@@ -19,6 +57,9 @@ namespace CompositePattern
         public FileTreatmentException(string msg) : base(msg) { }
     }
 
+    // Component
+    // ・LeafとCompositeを同一視するためのクラス
+    // ・LeafとCompositeの基底クラスとして実現する
     public abstract class Entry
     {
         public string Name { get; set; }
@@ -41,6 +82,9 @@ namespace CompositePattern
         }
     }
 
+    // Leaf
+    // ・「中身」を表すクラス
+    // ・中に他のクラスを入れることはできない
     public class File : Entry
     {
         public File(string name, int size)
@@ -51,10 +95,13 @@ namespace CompositePattern
 
         protected override void PrintList(string prefix)
         {
-            Console.WriteLine($"{prefix}/{this}");
+            Console.WriteLine($"{prefix}/{this.ToString()}");
         }
     }
 
+    // Composite
+    // ・「容器」を表すクラス
+    // ・LeafやCompositeを入れることができる
     public class Directry : Entry
     {
         private List<Entry> directry = new List<Entry>();
@@ -81,8 +128,8 @@ namespace CompositePattern
 
         protected override void PrintList(string prefix)
         {
-            Console.WriteLine($"{prefix}/{this}");
-            directry.ForEach(d => PrintList($"{prefix}/{d.Name}"));
+            Console.WriteLine($"{prefix}/{this.ToString()}");
+            directry.ForEach(d => Console.WriteLine($"{prefix}/{d.Name}"));
         }
     }
 }
