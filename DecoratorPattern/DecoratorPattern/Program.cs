f264b974a694622ce2740a5c75a120310628b244
toshi0607 <s.toshi0607@gmail.com>
2016-12-24 18:48:01 +0900

    add FullBorder class

--- DecoratorPattern/DecoratorPattern/Program.cs
+++ DecoratorPattern/DecoratorPattern/Program.cs
@@ -99,4 +99,50 @@ namespace DecoratorPattern
             return borderChar + display.GetRowText(row) + borderChar;
         }
     }
+
+    public class FullBorder : Border
+    {
+        public FullBorder(Display display) : base(display) { }
+        public override int Columns
+        {
+            get
+            {
+                return 1 + display.Columns + 1;
+            }
+        }
+
+        public override int Rows
+        {
+            get
+            {
+                return 1 + display.Rows + 1;
+            }
+        }
+
+        public override string GetRowText(int row)
+        {
+            if (row == 0)
+            {
+                return "+" + MakeLine('-', display.Columns) + "+";
+            }
+            else if (row == display.Rows + 1)
+            {
+                return "+" + MakeLine('-', display.Columns) + "+";
+            }
+            else
+            {
+                return "|" + display.GetRowText(row - 1) + "|";
+            }
+        }
+
+        private string MakeLine(char ch, int count)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                sb.Append(ch);
+            }
+            return sb.ToString();
+        }
+    }
 }
